79a99a06fff8ccae4883d53e29ad6918d25c2b81
Daniel C A <minedan1057@gmail.com>
2018-11-04 19:47:06 +0100

    LP MIAR Presentielle - CLEMENTE AGUIRRE Daniel

--- src/components/RuleList/RuleList.js
+++ src/components/RuleList/RuleList.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types'
 import Rule from '../Rule'
 
 class RuleList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      keyword: ''
+    }
+  }
+
   static propTypes = {
     rules: PropTypes.array,
     loadRules: PropTypes.func,
@@ -18,7 +25,37 @@ class RuleList extends React.Component {
 
   render() {
     const { rules } = this.props
-    return rules.map(rule => <Rule key={rule.id} rule={rule} />)
+    const filtered = rules.filter((rule) =>
+      rule.title.toLowerCase().includes(this.state.keyword.toLowerCase())
+    );
+    return (
+      <React.Fragment>
+        <Filter onTextChanged={text => {
+          console.log(text);
+          this.setState({
+            keyword: text
+          })
+        }} />
+        {filtered.map(rule => <Rule key={rule.id} rule={rule} />)}
+      </React.Fragment>
+    );
+  }
+}
+
+class Filter extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <input
+        className="form-control"
+        type="text"
+        onChange={event =>
+          this.props.onTextChanged(event.target.value)
+        }
+        placeholder="Search. . ." />
+        <span className="help-block text-center">Insert any word to find</span>
+      </div>
+    );
   }
 }
 
