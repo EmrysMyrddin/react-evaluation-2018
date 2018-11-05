ea35d14a600b6dfe60c671ad07d25de9c5c17b51
MegaRinku <cheezersd@gmail.com>
2018-11-05 00:13:37 -0600

    Update src/components/RuleList/RuleList.js

--- src/components/RuleList/RuleList.js
+++ src/components/RuleList/RuleList.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types'
 import Rule from '../Rule'
 
 class RuleList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      aChercher: ''
+    }
+  }
+
   static propTypes = {
     rules: PropTypes.array,
     loadRules: PropTypes.func,
@@ -18,7 +25,36 @@ class RuleList extends React.Component {
 
   render() {
     const { rules } = this.props
-    return rules.map(rule => <Rule key={rule.id} rule={rule} />)
+    const filtered = rules.filter((rule) =>
+      rule.title.toLowerCase().includes(this.state.aChercher.toLowerCase())
+    );
+    return (
+      <React.Fragment>
+        <Filter onTextChanged={text => {
+          this.setState({
+            aChercher: text
+          })
+        }} />
+        {filtered.map(rule => <Rule key={rule.id} rule={rule} />)}
+      </React.Fragment>
+    );
+  }
+}
+
+class Filter extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <input
+        className="form-control"
+        type="text"
+        onKeyUp={event =>
+          this.props.onTextChanged(event.target.value)
+        }
+        placeholder="Chercher?" />
+        <span className="help-block">Tapez quelque mot a chercher</span>
+      </div>
+    );
   }
 }
 
