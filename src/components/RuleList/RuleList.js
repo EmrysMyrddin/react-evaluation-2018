a8e5baf350243d6af6a91415a427b0119806eb0b
Benjamin-D <benjamin.alexis.dreano@gmail.com>
2018-11-05 17:28:58 +0100

    Filtre fonctionnel sur les titres Essai sur les radio buttons non concluant (autre classes) Mise en places de commentaires A améliorer

--- src/components/RuleList/RuleList.js
+++ src/components/RuleList/RuleList.js
@@ -7,6 +7,12 @@ class RuleList extends React.Component {
     rules: PropTypes.array,
     loadRules: PropTypes.func,
   }
+  constructor() {
+    super();
+    this.state = {
+      searched: ''
+  }
+}
 
   static defaultProps = {
     rules: [],
@@ -18,7 +24,48 @@ class RuleList extends React.Component {
 
   render() {
     const { rules } = this.props
-    return rules.map(rule => <Rule key={rule.id} rule={rule} />)
+
+    //ne fonctionne pas telquel avec d'autres éléments que title
+    const filtered = rules.filter((rule) =>
+      rule.title.toLowerCase().includes(this.state.searched.toLowerCase()));
+    return (
+
+      <React.Fragment>
+      //On définit le state afin de pouvoir faire la recherche sur le titre
+        <Filtre onTextChanged={text => {
+          this.setState({
+            searched: text
+          })
+        }} />
+        {filtered.map(rule => <Rule key={rule.id} rule={rule} />)}
+      </React.Fragment>
+    );
+  }
+}
+
+
+class Filtre extends React.Component {
+  /* A faire fonctionner, eventuellement via reducer
+  <form>
+    <input type="radio" id="titre" name="choix" value="title"/> Titre
+    <input type="radio" id="description" name="choix" value="description"/> Description
+    <input type="radio" id="likes" name="choix" value="likes"/> Likes
+    <input type="radio" id="dislikes" name="choix" value="dislikes"/> Dislikes
+    <input type="radio" id="tags" name="choix" value="tags"/> Tags
+  </form>*/
+render() {
+  return (
+    <div className="form-group">
+
+      <input
+      className="form-control"
+      type="text"
+      onChange={event =>
+        this.props.onTextChanged(event.target.value)
+      }
+      placeholder="Recherche" />
+    </div>
+  );
   }
 }
 
