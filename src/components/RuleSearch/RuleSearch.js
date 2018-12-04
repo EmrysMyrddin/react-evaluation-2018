2a91d4d3d36402a3f7a9141617bf31aa38379d12
Cédric HAMEL <cedric.hamel@etu.univ-nantes.fr>
2018-12-04 17:49:01 +0100

    Mise en place d'une fonction de tri

--- src/components/RuleSearch/RuleSearch.js
+++ src/components/RuleSearch/RuleSearch.js
@@ -21,11 +21,24 @@ class RuleSearch extends React.Component {
   }
 
   rechercher = (texte) => {
-    console.log("Recherche effectuée")
+    console.log(this.state.text)
+  }
+
+  trier = (test) => {
+    console.log("On entre dans la fonction")
+    console.log(test)
+    const { rules } = this.props
+    rules.forEach(function(rule){
+      if ( rule.title === test){
+        console.log("On entre dans la condition")
+        console.log(rule.id)
+        return <Rule key={rule.id} rule={rule}/>
+      }
+    })
   }
 
   render() {
-    return <div><input value={this.state.texte} onChange={e => this.setState({text: e.target.value})} type="text" placeholder="Recherche..."/><button onClick={this.rechercher}>Go</button></div>
+    return <div><input value={this.state.texte} onChange={e => this.setState({text: e.target.value})} type="text" placeholder="Recherche..."/><button onClick={this.trier(this.state.text)}>Go</button><div>{this.trier(this.state.text)}</div></div>
   }
 }
 
