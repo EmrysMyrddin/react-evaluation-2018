488f831b96883bac6ee416391ca7c505da4a5557
theo v <theo.viardin1@etu.univ-nantes.fr>
2018-12-04 17:12:07 +0100

    Fonction de recherche par titre

--- src/components/RuleList/RuleList.js
+++ src/components/RuleList/RuleList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Rule from '../Rule'
 
 class RuleList extends React.Component {
+  
   static propTypes = {
     rules: PropTypes.array,
     loadRules: PropTypes.func,
@@ -16,9 +17,41 @@ class RuleList extends React.Component {
     this.props.loadRules()
   }
 
-  render() {
+  handleChange(e) {
     const { rules } = this.props
-    return rules.map(rule => <Rule key={rule.id} rule={rule} />)
+    let newList = []
+
+    if (e.target.value !== "") {
+      newList = rules.filter(item => {
+        const lc = item.title.toLowerCase()
+        const filter = e.target.value.toLowerCase()
+        return lc.includes(filter)
+      })
+    } else {
+      newList = rules
+    }
+    this.setState({
+      rules: newList
+    })
+  }
+
+
+  render() {
+    this.handleChange = this.handleChange.bind(this)
+    let { rules } = this.props
+    if (this.state != null && this.state.rules != null) {
+      rules = this.state.rules
+    }
+    return [
+      <input 
+        class="form-control mr-sm-2" 
+        type="text"
+        onChange={this.handleChange} 
+        placeholder="Search" 
+        aria-label="Search"
+      />,
+      rules.map(rule => <Rule key={rule.id} rule={rule} />)
+    ]
   }
 }
 
